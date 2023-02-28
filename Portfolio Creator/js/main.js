1f5196ea911ee03b20959b202f614745692a9e8e
djalil2004 <122861717+djalil2004@users.noreply.github.com>
2023-02-28 22:40:21 +0300

    Add files via upload
    Готовый сайт)

---
+++ Portfolio Creator/js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+
+  $('.testimonials__slider').slick({
+    arrows: false,
+    waitForAnimate: false,
+   
+  })
+  $('.testimonials__prev').on('click', function (e) {
+    e.preventDefault()
+    $('.testimonials__slider').slick('slickPrev')
+  })
+  $('.testimonials__next').on('click', function (e) {
+    e.preventDefault()
+    $('.testimonials__slider').slick('slickNext')
+  })
+})
